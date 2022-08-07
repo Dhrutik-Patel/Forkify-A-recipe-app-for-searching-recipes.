9967e4b23e04ec93facf86e1ef7ab4ff0ccdf74b
Dhrutik-Patel <dhrutikpatel2017@gmail.com>
2022-08-07 13:04:08 +0530

    v1.0.0

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e2c46640-6d63-4c60-8771-7b65174860dc';
+export const MODAL_CLOSE_SEC = 2.5;
